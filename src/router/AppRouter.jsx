25ba77045e2d7ab0595c664ca8e1bdd9a42a5ca7
S-404 <sudarev.andrey.i@gmail.com>
2022-04-18 22:23:53 +0300

    upd AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,33 @@
 import React from "react"
-import {BrowserRouter as Router} from "react-router-dom"
-import MyNavBar from "../components/UI/MyNavBar"
-import AppRoutes from "./AppRoutes"
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom"
+import {withNavbarRoutes, withoutNavbarRoutes} from "./routes"
+import MyNavBar from "../components/UI/myNavBar/MyNavBar"
 
 const AppRouter = () => {
     return (
         <Router>
-            <MyNavBar/>
-            <AppRoutes/>
+            <Switch>
+                {withoutNavbarRoutes.map(route => (
+                    <Route
+                        exact={route?.isExact}
+                        key={route.name}
+                        path={route.path}
+                        component={route.component}
+                    />
+                ))}
+                <>
+                    <MyNavBar/>
+                    {withNavbarRoutes.map(route => (
+                        <Route
+                            exact={route?.isExact}
+                            key={route.name}
+                            path={route.path}
+                            component={route.component}
+                        />
+                    ))}
+                </>
+                <Redirect exact to={"/"}/>
+            </Switch>
         </Router>
     )
 }
