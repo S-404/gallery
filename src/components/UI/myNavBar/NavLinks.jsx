051084ec3b5da917bf28d692444fe8b4f78dfa62
S-404 <sudarev.andrey.i@gmail.com>
2022-04-18 22:23:07 +0300

    upd MyNavBar + add NavLinks

---
+++ src/components/UI/myNavBar/NavLinks.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {Nav, Navbar} from "react-bootstrap"
+import {Link} from "react-router-dom"
+import {withNavbarRoutes} from "../../../router/routes"
+
+const NavLinks = () => {
+    return (
+        <Nav className="m-3">
+            <>
+                {withNavbarRoutes.map(route => (
+                        <Navbar.Toggle
+                            as={Link}
+                            to={route.path}
+                        >
+                            {route.name}
+                        </Navbar.Toggle>
+                ))}
+            </>
+        </Nav>
+    )
+}
+
+export default NavLinks
