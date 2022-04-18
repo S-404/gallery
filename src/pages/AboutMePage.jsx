dcd47c6a16589715924ed44259e20b284178a438
S-404 <sudarev.andrey.i@gmail.com>
2022-04-18 22:25:35 +0300

    upd page AboutMePage

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import Person from "../components/person/Person"
+import AboutMe from "../components/person/AboutMe"
 
 const AboutMePage = () => {
     return (
-        <div>
-            about me
+        <div className="d-flex h-75 w-100 p-5 flex-md-row flex-column align-items-center justify-content-center">
+            <Person/>
+            <AboutMe/>
         </div>
     )
 }
