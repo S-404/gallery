42abbdbf596a5a28cd3e92922964fd09b74317dd
S-404 <sudarev.andrey.i@gmail.com>
2022-04-19 00:36:58 +0300

    upd styles

--- src/components/imageCard/ImageCard.jsx
+++ src/components/imageCard/ImageCard.jsx
@@ -4,7 +4,7 @@ import {Card} from "react-bootstrap"
 const ImageCard = ({image}) => {
     return (
         <Card border="secondary" body bg="light" text="dark">
-            <Card.Img variant="top" src={image?.url}/>
+            <Card.Img className='w-100' variant="top" src={image?.url}/>
             <Card.Body>
                 <Card.Title>{image?.title}</Card.Title>
                 <Card.Text>id: {image?.id}</Card.Text>
